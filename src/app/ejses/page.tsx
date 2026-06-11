fff8e7b0effbf8f6bb9e50d67f7e5de05a8bb460
kousei0413 <kousei085875900@outlook.jp>
2026-06-11 19:47:10 +0900

    Update page.tsx

--- src/app/ejses/page.tsx
+++ src/app/ejses/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       {/* タイポグラフィ（組織概要・お知らせの文字データのみに集約） */}
       <div className="space-y-6">
         <div className="text-xs font-bold tracking-widest text-blue-600 uppercase">
-          Expancoov Project Portal
+         what is cps3?????????
         </div>
         
         <h1 className="text-4xl sm:text-6xl font-black text-gray-950 tracking-tight leading-tight">
@@ -17,13 +17,11 @@ export default function Home() {
         </h1>
         
         <p className="text-gray-500 text-sm sm:text-base max-w-xl leading-relaxed font-medium">
-          node.jsなどを用いたコードを開発、shadowcompnyaddonのガンパックを開発している組織です
+          CPシステムIII（シーピーシステム スリー）とは、1996年に発売・稼働開始したカプコン開発のアーケードゲーム基板である。この基板がカプコンの開発した最後のアーケードゲーム用システム基板となり、以降はセガ製のNAOMI基板など、他社製の基板を採用するよう方向にシフトしていくこととなる。日本国外を中心にCPS-3と略称されることがある（以降、記事中ではこの略称を用いる）。
         </p>
         
         <p className="text-gray-500 text-sm sm:text-base max-w-xl leading-relaxed font-medium">
-          【お知らせ】<br />
-          ガンパックをスキンパックだと一部の人が誤解しているようです、<br />
-          ガンパックはscaddonに新たな銃機を追加するもので、スキンパックはまた別のものです。
+          
         </p>
       </div>
 
